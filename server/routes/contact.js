0c517ba715cf0993ba3b16424f6041f9ef12c5cf
kranthikumar <kranthiveer7@gmail.com>
2019-04-06 23:43:19 -0400

    added comments in contact

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -2,8 +2,10 @@
 /* Project Name: COMP308 Assignment2
    Name: Kranthi kumar G J
    Student id: 300976059
-   Date: 05-APR-2019 */
-   
+   Date: 05-APR-2019 
+   file name:route.js
+   */
+
 let express = require('express');
 let router = express.Router();
 
